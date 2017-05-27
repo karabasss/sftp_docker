cd2589f6b8764343cbd691e8d40cbe81fd1befd3
BASS <bass@debian>
2017-05-27 14:39:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+SFTP watcher
+
+To start run start.sh
+
+
+Script supports only yum and apt systems.
+You must have sudo rights
---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+YUM_CMD=$(which yum)
+APT_CMD=$(which apt)
+
+if [[ ! -z $YUM_CMD ]]; then
+    sudo yum install -y python-pip
+elif [[ ! -z $APT_CMD ]]; then
+    sudo apt install -y python-pip  
+
+else
+    echo "error, script supports only yum and apt"
+    exit 1;
+fi
+
+sudo pip install ansible
+ansible-playbook docker.yml
+
