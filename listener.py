8b7b99846296138a1b13ac1cd972199b3bc55b2f
karabasss <karabass.com@gmail.com>
2017-05-28 14:33:05 +0000

    script fixes

--- listener.py
+++ listener.py
@@ -1,1 +1,28 @@
-print "Listener started!"
+import sys, time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+print "Listener started! Press ctrl+c to interrupt."
+
+class Listener(FileSystemEventHandler):
+  def on_created(self, event):
+        self.process(event)
+
+  def process(self, event):
+        print event.src_path
+
+path = "/opt/upload"
+event_handler = FileSystemEventHandler()
+observer = Observer()
+observer.schedule(Listener(), path, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
+
+
